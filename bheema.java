5b8171953131a8dae2d02c1b8b54d551da1d051b
Abhiavishek <avi74500@gmail.com>
2023-07-27 22:11:21 +0530

    successfully commited

--- bheema.java
+++ bheema.java
@@ -1,9 +1,19 @@
+import java.util.*;
 Interface Bheema{
   public void bheema();
 }
 
 public class BheemaImpli implements Bheema{
-   public void bheema(){
-   System.out.println("Lady Don");
+   public void bheema(String str){
+   System.out.println(str);
+}
+}
+class something {
+  public static void main(String[] args){
+  Scanner scn = new Scanner(System.in);
+  String str = s.nextLine();
+  BheemaImpli b = new BheemaImpli();
+  b.bheema("Lady Don");
+  
 }
 }
